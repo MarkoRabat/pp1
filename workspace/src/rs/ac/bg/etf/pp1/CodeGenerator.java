8a955026c86f559edd9d6ca9b12b966eaf0675fa
Marko Rabat <rm200196d@student.etf.bg.ac.rs>
2024-01-31 23:30:55 +0100

    added variable initialization, bool and char constants; fixed mainPc=pc in main instread of 0 - former skipped over all code before main

--- workspace/src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ workspace/src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -7,14 +7,37 @@ import rs.etf.pp1.symboltable.concepts.*;
 import rs.ac.bg.etf.pp1.ast.*;
 
 public class CodeGenerator extends VisitorAdaptor {
-	private int mainPc;
+	private int mainPc = 0;
 	public int getMainPc() { return mainPc; }
+	
+	public void visit(IIdentDecl iIdentDecl) {
+		Code.store(iIdentDecl.obj);
+	}
+	
+	public void visit(NInit nInit) {
+		Obj con = Tab.insert(Obj.Con, "$", nInit.obj.getType());
+		con.setLevel(0); con.setAdr(nInit.getN1()); Code.load(con);
+	}
+
+	public void visit(CInit cInit) {
+		Obj con = Tab.insert(Obj.Con, "$", cInit.obj.getType());
+		con.setLevel(0);
+		con.setAdr(cInit.getC1().charAt(1));
+		Code.load(con);
+	}
+
+	public void visit(BInit bInit) {
+		Obj con = Tab.insert(Obj.Con, "$", bInit.obj.getType());
+		con.setLevel(0);
+		con.setAdr((bInit.getB1().equals("true") ? 1 : 0));
+		Code.load(con);
+	}
 	
 	public void visit(MethodDeclaration methodDeclaration) {
 		Code.put(Code.exit); Code.put(Code.return_); }
 	
 	public void visit(MethName methName) {
-		mainPc = Code.pc; methName.obj.setAdr(Code.pc);
+		methName.obj.setAdr(Code.pc);
 		SyntaxNode methodDecl = methName.getParent();
 		VarCounter countLocals = new VarCounter();
 		methodDecl.traverseTopDown(countLocals);
@@ -64,10 +87,28 @@ public class CodeGenerator extends VisitorAdaptor {
 		Obj con = Tab.insert(Obj.Con, "$", numberConst.struct);
 		con.setLevel(0); con.setAdr(numberConst.getN1()); Code.load(con);
 	}
+
+	public void visit(CharacterConst characterConst) {
+		Obj con = Tab.insert(Obj.Con, "$", characterConst.struct);
+		con.setLevel(0);
+		con.setAdr(characterConst.getC1().charAt(1));
+		Code.load(con);
+	}
+
+	public void visit(BooleanConst booleanConst) {
+		Obj con = Tab.insert(Obj.Con, "$", booleanConst.struct);
+		con.setLevel(0);
+		con.setAdr((booleanConst.getB1().equals("true") ? 1 : 0));
+		Code.load(con);
+	}
 	
 	public void visit(Ident ident) {
 		if (ident.getParent().getClass() == SDesignAsign.class) return;
 		Code.load(ident.obj);
 	}
 
+	public void visit(NsIdent nsIdent) {
+		if (nsIdent.getParent().getClass() == SDesignAsign.class) return;
+		Code.load(nsIdent.obj);
+	}
 }
